a39ae933cb6f47c6d56abfa0ca762a6e358a8712
mahmoud-salah18 <modybale18@gmail.com>
2025-08-19 22:05:20 +0300

    Task 2

---
+++ lib/core/colors.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class AppColors{
+  static const Color primaryColor =  Color(0xffeb1555);
+  static const Color secondaryColor =  Color(0xff1d1e33);
+  static const Color bgColor =  Color(0xff0a0c21);
+  static const Color greyColor =  Color(0xff4a505c);
+  static const Color whiteColor =  Colors.white;
+}
---
+++ lib/page/result_screen.dart
@@ -0,0 +1,118 @@
+import 'package:bmi_calculator/core/colors.dart';
+import 'package:flutter/material.dart';
+
+class ResultScreen extends StatelessWidget {
+  const ResultScreen({super.key, required this.result});
+  final double result;
+
+  String getCategory() {
+    if (result < 18.5) {
+      return "UNDERWEIGHT";
+    } else if (result < 25) {
+      return "NORMAL";
+    } else if (result < 30) {
+      return "OVERWEIGHT";
+    } else {
+      return "OBESE";
+    }
+  }
+
+  String getMessage() {
+    if (result < 18.5) {
+      return "You are underweight,\nTake care and eat well ";
+    } else if (result < 25) {
+      return "Your body weight is absolutely normal,\nGood job! ";
+    } else if (result < 30) {
+      return "You are overweight,\nConsider exercising ";
+    } else {
+      return "You are obese,\nConsult a doctor and stay strong ";
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.deepOrange,
+        foregroundColor: Colors.black,
+      ),
+      backgroundColor: AppColors.secondaryColor,
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          spacing: 50,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              "Your result",
+              style: TextStyle(
+                fontSize: 30,
+                fontWeight: FontWeight.w600,
+                color: Colors.deepOrange,
+              ),
+            ),
+            SizedBox(height: 30),
+            Expanded(
+              child: Center(
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    Text(
+                      getCategory(),
+                      style: TextStyle(
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.deepOrange,
+                      ),
+                    ),
+                    SizedBox(height: 10),
+                    Text(
+                      result.toStringAsFixed(2),
+                      style: TextStyle(
+                        fontSize: 60,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.deepOrange,
+                      ),
+                    ),
+                    SizedBox(height: 10),
+                    Text(
+                      getMessage(),
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        fontSize: 15,
+                        fontWeight: FontWeight.w300,
+                        color: Colors.white,
+                      ),
+                    ),
+                  ],                  
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+      bottomNavigationBar: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: SizedBox(
+          width: double.infinity,
+          height: 60,
+          child: ElevatedButton(
+            onPressed: () {
+              Navigator.pop(context);
+            },
+            child: Text(
+              "Recalculate",
+              style: TextStyle(color: Colors.black, fontSize: 18),
+            ),
+            style: ElevatedButton.styleFrom(
+              backgroundColor: Colors.deepOrange,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(10),
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
